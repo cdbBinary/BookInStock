5b1c4dfc14bc2e19155f4329604f943c4d26faae
charlie.brown <cdb.binary@gmail.com>
2018-06-17 23:44:03 -0400

    upcase filename

--- stock_stats.rb
+++ stock_stats.rb
@@ -3,7 +3,7 @@ require_relative 'csv_reader'
 reader = CsvReader.new
 
 ARGV.each do |csv_file_name|
-	STDERR.puts "Processing #{csv_file_name}"
+	STDERR.puts "Processing #{csv_file_name.upcase}"
 	reader.read_in_csv_data(csv_file_name)
 end
 
